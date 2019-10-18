1ef1495b74adc5f4f8eb0255ee88cf001eb39ccf
Taljjaa <jenken131@gmail.com>
2019-10-18 10:45:26 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
+
+    def intialize()
+    
+    end
+
+    def learn(str)
+        @knowledge.push(str)
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
